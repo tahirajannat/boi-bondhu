d6b6108b4b30a31112156fd47acbe3cf257cfb9d
Tahira Jannat <tahirajannat.webappick@gmail.com>
2023-11-28 10:55:33 +0600

    BOIB-3: category & area filter done

--- src/components/views/product/Archive.jsx
+++ src/components/views/product/Archive.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectProductShop } from '../../../redux/reducers/productShopSlice';
 import BookCard from '../../common/BookCard';
+import NotFound from '../../common/NotFound';
 import Sidebar from '../Sidebar';
 
-// Define a separate filter function
-
-const Archive = () => {
+export default function Archive() {
     const productShop = useSelector(selectProductShop);
     const dispatch = useDispatch();
     const [books, setBooks] = useState(productShop.allBooks);
@@ -21,6 +20,7 @@ const Archive = () => {
                 : prevAreas.filter((area) => area !== value)
         );
     };
+
     const handleCategoryChange = (event) => {
         const { value, checked } = event.target;
         setSelectedCategories((prevCategories) =>
@@ -29,41 +29,34 @@ const Archive = () => {
                 : prevCategories.filter((category) => category !== value)
         );
     };
-    useEffect(() => {
-        // Use the filter function to update the books state
-        setBooks(filterBooksByAreas(productShop.allBooks, selectedAreas));
-
-        console.log('selectedCategories:', selectedAreas);
-    }, [selectedAreas]);
 
     useEffect(() => {
         // Use the filter function to update the books state
         setBooks(
-            filterBooksByCategories(productShop.allBooks, selectedCategories)
+            filterBooks(productShop.allBooks, selectedAreas, selectedCategories)
         );
+    }, [selectedAreas, selectedCategories]);
 
-        console.log('selectedCategories:', selectedCategories);
-    }, [selectedCategories]);
+    const filterBooks = (books, selectedAreas, selectedCategories) => {
+        let filteredBooks = books;
 
-    const filterBooksByAreas = (books, selectedAreas) => {
         if (selectedAreas.length > 0) {
-            return books.filter((book) => selectedAreas.includes(book.area));
-        } else {
-            return books;
+            filteredBooks = filteredBooks.filter((book) =>
+                selectedAreas.includes(book.area)
+            );
         }
-    };
-    const filterBooksByCategories = (books, selectedCategories) => {
+
         if (selectedCategories.length > 0) {
-            return books.filter((book) =>
+            filteredBooks = filteredBooks.filter((book) =>
                 selectedCategories.includes(book.category)
             );
-        } else {
-            return books;
         }
+
+        return filteredBooks;
     };
 
     return (
-        <div className='container mx-auto px-20 grid grid-cols-12 gap-6'>
+        <div className='container  mx-auto px-20 grid grid-cols-12 gap-6'>
             <div className='col-span-3'>
                 <Sidebar
                     onCategoryChange={handleCategoryChange}
@@ -75,11 +68,15 @@ const Archive = () => {
                 <h2 className='text-xl font-bold text-gray-900'></h2>
 
                 <div className='grid grid-cols-3 gap-6 mt-8'>
-                    <BookCard items={books} />
+                    {books.length > 0 ? (
+                        <BookCard items={books} />
+                    ) : (
+                        <div className='col-span-3 mt-32'>
+                            <NotFound />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
     );
-};
-
-export default Archive;
+}
